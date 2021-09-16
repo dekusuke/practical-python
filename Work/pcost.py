048889d2cf9d55534d328f20d64cdd9b240c46ec
dekusuke <iylian9@naver.com>
2021-09-16 16:39:39 +0900

    next is ex 1.32

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,19 @@
-# pcost.py
-#
-# Exercise 1.27
+def portfolio_cost(filename):
+
+    with open('Data/missing.csv', 'rt') as f:
+        headers = next(f)
+        total = 0
+        try:
+            for line in f:
+                row = line.split(',')
+                number = int(row[1])
+                price = float(row[2])
+                cost = number * price
+                total = total + cost
+            return total
+        except ValueError:
+            print('something is missing')
+
+
+cost2 = portfolio_cost('Data/missing.csv')
+print('Total cost:', cost2)
