9b871dbaab2eeca8bdadeb65be0ae5dc3e150b55
dekusuke <iylian9@naver.com>
2021-09-29 16:40:13 +0900

    next is 03-03

--- Work/report.py
+++ Work/report.py
@@ -27,26 +27,28 @@ def read_prices(filename):
     return prices
     f.close()
 
-def make_report(list_port, dic_pri):
+def print_report(list_port, dic_pri):
     report = []
     for dic in list_port:
         add = (dic['name'], dic['shares'], dic_pri[dic['name']], dic_pri[dic['name']] - dic['price'])
         report.append(add)
+    print(report)
 
     return report
 
-portfolio = read_portfolio('Data/portfolio.csv')
-prices = read_prices('Data/prices.csv')
-report = make_report(portfolio, prices)
-headers = ('Name', 'Shares', 'Price', 'Change')
+def portfolio_report(portfolio_filename, prices_filename):
+    portfolio = read_portfolio(portfolio_filename)
+    prices = read_prices(prices_filename)
+    report = print_report(portfolio, prices)
+    headers = ('Name', 'Shares', 'Price', 'Change')
 
-print('%10s %10s %10s %10s' % headers)
-print(('-' * 10 + ' ') * len(headers))
-    
+    print('%10s %10s %10s %10s' % headers)
+    print(('-' * 10 + ' ') * len(headers))
+        
 
-for name, shares, price, change in report:
-    price = '$' + str(price)
-    print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
+    for name, shares, price, change in report:
+        price = '$' + str(price)
+        print(f'{name:>10s} {shares:>10d} {price:>10s} {change:>10.2f}')
 
 
 
